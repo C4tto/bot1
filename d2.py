060f8da569ccdde86b6b84d9de04bbf94bd75741
Catto <139864993+C4tto@users.noreply.github.com>
2025-06-19 23:45:47 -0300

    Update d2.py

--- d2.py
+++ d2.py
@@ -5,7 +5,6 @@ import requests
 st.set_page_config(page_title="Chatbot con Groq", layout="centered")
 st.title("Chatbot con Groq")
 
-# api_key
 key = "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"
 
 # modelos
